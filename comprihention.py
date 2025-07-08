2b4add6e0f5c90cd903423ae4783343d55e49386
suvamshrestha <suvamshrestha93@gmail.com>
2025-07-08 19:07:54 +0545

    Add files via upload

---
+++ comprihention.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+odds = [i for i in range(n) if i % 2 != 0]
+print(odds)
+fruits = ["apple", "banana", "mango"]
+new_fruits = [f.capitalize() for f in fruits]
+print(new_fruits)
+
